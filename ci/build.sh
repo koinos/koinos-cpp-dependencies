d43e7b6cd71509b81696c020f7eff816dc7d3c88
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2021-01-06 18:57:34 -0800

    Get 18.04 python version

--- ci/build.sh
+++ ci/build.sh
@@ -4,12 +4,9 @@ set -e
 set -x
 
 which python
+python --version
 
 mkdir build
 cd build
 
-if [ "$RUN_TYPE" = "test" ]; then
-   cmake -DCMAKE_BUILD_TYPE=Release ..
-elif [ "$RUN_TYPE" = "coverage" ]; then
-   cmake -DCMAKE_BUILD_TYPE=Debug -DCOVERAGE=ON ..
-fi
+cmake -DCMAKE_BUILD_TYPE=Release ..
