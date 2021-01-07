155659f61e8c83d2a26a6480963f947217e8b7ac
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2021-01-07 11:31:01 -0800

    Try a different HUNTER_PYTHON_LOCATION

--- ci/build.sh
+++ ci/build.sh
@@ -6,7 +6,7 @@ set -x
 mkdir build
 cd build
 
-export HUNTER_PYTHON_LOCATION=/opt/pyenv/shims
+export HUNTER_PYTHON_LOCATION=/usr/bin
 echo $HUNTER_PYTHON_LOCATION
 
 cmake -DCMAKE_BUILD_TYPE=Release ..
