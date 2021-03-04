41ad2aced3bf1dd12aeb6c5db502cbb2aef30366
Steve Gerbino <steve@gerbino.co>
2021-03-04 01:57:39 -0500

    Update dependencies

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,10 +1,14 @@
 hunter_config(Boost
    VERSION ${HUNTER_Boost_VERSION}
-   CMAKE_ARGS USE_CONFIG_FROM_BOOST=ON
+   CMAKE_ARGS
+      USE_CONFIG_FROM_BOOST=ON
+      Boost_USE_STATIC_LIBS=ON
+      Boost_NO_BOOST_CMAKE=ON
 )
 
 hunter_config(rocksdb
-   VERSION ${HUNTER_rocksdb_VERSION}
+   URL "https://github.com/facebook/rocksdb/archive/v6.15.2.tar.gz"
+   SHA1 "daf7ef3946fd39c910acaaa57789af8515b39251"
    CMAKE_ARGS
       WITH_TESTS=OFF
       WITH_TOOLS=OFF
@@ -18,3 +22,16 @@ hunter_config(rocksdb
       CMAKE_CXX_FLAGS=-fvisibility=hidden
       CMAKE_C_FLAGS=-fvisibility=hidden
 )
+
+hunter_config(rabbitmq-c
+   URL "https://github.com/alanxz/rabbitmq-c/archive/b8e5f43b082c5399bf1ee723c3fd3c19cecd843e.tar.gz"
+   SHA1 "35d4ce3e4f0a5348de64bbed25c6e1df72da2594"
+   CMAKE_ARGS
+      ENABLE_SSL_SUPPORT=OFF
+)
+
+hunter_config(libsecp256k1
+   URL "https://github.com/soramitsu/soramitsu-libsecp256k1/archive/c7630e1bac638c0f16ee66d4dce7b5c49eecbaa5.tar.gz"
+   SHA1 "0534fa8948f279b26fd102905215a56f0ad7fa18"
+)
+
