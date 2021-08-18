75bab19901015f7e9ec8b62928dfb19a507b953d
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2021-08-18 15:45:55 -0700

    Bump dep versions

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,9 +1,16 @@
 hunter_config(Boost
-   VERSION "1.72.0-p1"
+VERSION ${HUNTER_Boost_VERSION}
+CMAKE_ARGS
+   USE_CONFIG_FROM_BOOST=ON
+   Boost_USE_STATIC_LIBS=ON
+   Boost_NO_BOOST_CMAKE=ON
+)
+
+hunter_config(Protobuf
+   VERSION ${HUNTER_Protobuf_VERSION}
    CMAKE_ARGS
-      USE_CONFIG_FROM_BOOST=ON
-      Boost_USE_STATIC_LIBS=ON
-      Boost_NO_BOOST_CMAKE=ON
+      CMAKE_CXX_FLAGS=-fvisibility=hidden
+      CMAKE_C_FLAGS=-fvisibility=hidden
 )
 
 hunter_config(rocksdb
